10d5427c22e6e38c16e4aa5eb9eb4c2f8af98987
shuwangp <Zlag112288@gmail.com>
2025-09-17 00:43:26 +0700

    add netlify.toml and set API proxy

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
-const API_BASE_URL = 'http://ai-powered-backend-env-2.eba-ybwywm7t.ap-southeast-1.elasticbeanstalk.com/api';
+// const API_BASE_URL = 'http://ai-powered-backend-env-2.eba-ybwywm7t.ap-southeast-1.elasticbeanstalk.com/api';
+const API_BASE_URL = "/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
